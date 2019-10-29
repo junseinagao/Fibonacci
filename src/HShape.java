74d6a5165a89c8875979d04f5513ea5367402b73
Kobalent <56064674+Kobalent@users.noreply.github.com>
2019-10-29 12:11:46 -0700

    Added a constructor/ draw override
    I think the code will need some rework to do the recursive features, but for the purposes of this assignment it seems to get the job done.

--- src/HShape.java
+++ src/HShape.java
@@ -1,4 +1,39 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class HShape {
-
+public class HShape extends AbstractShape{
+	private int size;
+	
+	public HShape(int x, int y, Color c, int size) {
+		this.x = x;
+		this.y = y;
+		this.c = c;
+		this.size = size;
+	}
+	
+	public void draw(Graphics g){
+		g.setColor(c);
+		// Fills the spaces corresponding to the adequate squares
+		g.fillRect(x, y, size, size);
+		g.fillRect(x-size, y-size, size, size);
+		g.fillRect(x-size, y, size, size);
+		g.fillRect(x-size, y+size, size, size);
+		g.fillRect(x+size, y-size, size, size);
+		g.fillRect(x+size, y, size, size);
+		g.fillRect(x+size, y+size, size, size);
+	}
+	
+  /*
+  * I tested the code by adding a few HShapes to the model in the MainClass
+  *   Shape h1 = new HShape(85, 100, Color.GREEN, 60);
+  *   Shape h2 = new HShape(750, 30, Color.GREEN, 10);
+  *   Shape h3 = new HShape(525, 140, Color.GREEN, 15);
+  *   Shape h4 = new HShape(310, 260, Color.GREEN, 40);
+  *   model.addShape(h1);
+  *   model.addShapw(h2);
+  *   model.addShape(h3);
+  *   model.addShape(h4);
+  *
+  */
+	
 }
